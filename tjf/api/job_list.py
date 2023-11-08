97ae4f868512579e926291c1ade515e00c3b2a5f
Raymond Ndibe <rolisaemeka-ctr@wikimedia.org>
2023-11-08 23:43:38 +0000

    Revert "api: Fix mount type validation logic"
    This reverts commit 8f5e1f6003eac0d12a0c865d3dbe0318a8fd85dc

--- tjf/api/job_list.py
+++ tjf/api/job_list.py
@@ -73,9 +73,9 @@ class JobListResource(Resource):
                 "A job with the same name exists already", http_status_code=409
             )
 
-        if image.type != ImageType.BUILDPACK and not args.mount.supports_non_buildservice:
+        if args.mount.supports_non_buildservice and image.type != ImageType.BUILDPACK:
             raise TjfValidationError(
-                f"Mount type {args.mount.value} is only supported for build service images"
+                f"Mount type {image.type} is only supported for build service images"
             )
         if args.filelog and not image.type.supports_file_logs():
             raise TjfValidationError("Build service images do not support file logs")
