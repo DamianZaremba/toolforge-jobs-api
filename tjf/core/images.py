e8c1b9126080014de0ec0146c64a8c7d7e679c0f
David Caro <dcaro@wikimedia.org>
2026-02-23 15:43:16 +0100

    images: return the hash if it was passed
    This adds the hash to the returned image if a hash was passed to
    create the job.
    
    Depends-On: !271\
    Bug: T417503\
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- tjf/core/images.py
+++ tjf/core/images.py
@@ -123,30 +123,41 @@ class Image(BaseModel):
             image_state = HARBOR_IMAGE_STATE
         else:
             project = ""
+            image_type = ImageType.STANDARD
 
         # we don't really use tags yet :/, expecting it to be 'latest'
         image_name = image_name_with_tag_and_project.split(":", 1)[0]
 
-        if project:
+        if project and "tool-" in project:
             # we allow tools to use other tools images
             tool_name = project.split("tool-", 1)[-1]
 
         all_images = get_images(tool=tool_name, use_harbor_cache=use_harbor_cache)
 
+        found_image = None
         for image in all_images:
             if image.short_name in (image_name, image_name_with_tag_and_project):
                 if not digest or digest == image.digest:
-                    image.digest = digest
-                    return image
+                    found_image = image
+                    break
                 else:
                     LOGGER.debug(f"Skipping image due to digest, looking for {digest} got {image}")
             elif (
                 image_name_with_tag_and_digest in image.aliases
                 or url_or_name == image.to_full_url()
             ):
-                # the digest would have been matched already in the aliases or the container
-                image.digest = digest
-                return image
+                found_image = image
+                break
+
+        if found_image:
+            LOGGER.debug(f"Found matching image {found_image} for {image_name}")
+            new_image = found_image.model_copy()
+            # the digest would have been matched already in the aliases or the short_name
+            new_image.digest = digest
+            # if a digest was passed to us explicitly, then add it to the short name too
+            if digest and digest in image_name_with_tag_and_digest:
+                new_image.short_name = image_name_with_tag_and_digest
+            return new_image
 
         LOGGER.debug(
             f"Unable to find matching image for {url_or_name}, available images for tool {tool_name}: {all_images}"
@@ -154,17 +165,33 @@ class Image(BaseModel):
         if raise_for_nonexisting:
             raise TjfValidationError(f"No such image '{url_or_name}'")
 
+        # this is an image that does not match any known one
+        # this might be because it's been deprecated (and not on the list anymore)
         aliases = []
         # If we have a digest, keep it as an alias too
         if digest:
             aliases.append(image_name_with_tag_and_digest)
 
-        # this is a buildpack image
-        host = _get_harbor_config().host
-        path, tag = image_name_with_tag_and_project.split(":", 1)
-        return cls(
+        if image_type == ImageType.BUILDPACK:
+            host = _get_harbor_config().host
+            if ":" in image_name_with_tag_and_project:
+                path, tag = image_name_with_tag_and_project.split(":", 1)
+                tag = tag.split("@", 1)[0]
+            else:
+                path = image_name_with_tag_and_project
+                tag = "latest"
+        else:
+            path = url_or_name
+            host = tag = ""
+            if "/" in path and path.startswith("docker-registry."):
+                host, path = path.split("/", 1)
+            if ":" in path:
+                path, tag = path.split(":", 1)
+                tag = tag.split("@", 1)[0]
+
+        new_image = cls(
             type=image_type,
-            short_name=image_name_with_tag_and_project,
+            short_name=image_name_with_tag_and_digest,
             aliases=aliases,
             host=host,
             path=path,
@@ -172,6 +199,8 @@ class Image(BaseModel):
             state=image_state,
             digest=digest,
         )
+        LOGGER.debug(f"Got unknown image {new_image}")
+        return new_image
 
 
 def _get_hostless_url(url: str) -> str:
@@ -183,6 +212,9 @@ def _get_hostless_url(url: str) -> str:
         image_name_with_tag = url.removeprefix("docker-registry.tools.wmflabs.org/")
     elif url.startswith("docker-registry.svc.toolforge.org/"):
         image_name_with_tag = url.removeprefix("docker-registry.svc.toolforge.org/")
+    elif "." in url.split("/", 1)[0]:
+        # trying to match unknown host, for old non-supported images coming from k8s
+        image_name_with_tag = url.split("/", 1)[-1]
     else:
         image_name_with_tag = url
     return image_name_with_tag
