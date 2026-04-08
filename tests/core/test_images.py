f52c9342a3ca230bfe96e79741fafa94d372ae6f
raymond-ndibe <rndibe@wikimedia.org>
2026-04-08 20:12:03 +0100

    improve image parsing tests
    * improve from_short_name_or_url method docstring
    * add php8.4 to FAKE_IMAGE_CONFIG for testing undeprecated image with no alias
    * flatten test array passed to parameterise in test_images.py
    * general test refactor
    
    Bug: T415322
    Signed-off-by: raymond-ndibe <rndibe@wikimedia.org>

--- tests/core/test_images.py
+++ tests/core/test_images.py
@@ -1,5 +1,3 @@
-import pytest
-
 from tests.helpers.fakes import FAKE_HARBOR_HOST
 from tests.test_utils import cases
 from tjf.core.images import (
@@ -16,179 +14,289 @@ def test_available_images_len(fake_images):
 
 IMAGE_NAME_TESTS = [
     [
-        "node12",
-        Image(
-            short_name="node12",
-            type=ImageType.STANDARD,
-            host="docker-registry.tools.wmflabs.org",
-            path="toolforge-node12-sssd-base",
-            tag="latest",
-            aliases=["tf-node12", "tf-node12-DEPRECATED"],
-            digest="",
-            state="deprecated",
-        ),
+        "short name for node12 image",
+        [
+            "node12",
+            Image(
+                short_name="node12",
+                type=ImageType.STANDARD,
+                host="docker-registry.tools.wmflabs.org",
+                path="toolforge-node12-sssd-base",
+                tag="latest",
+                aliases=["tf-node12", "tf-node12-DEPRECATED"],
+                state="deprecated",
+            ),
+        ],
     ],
     [
-        "tf-node12",
-        Image(
-            short_name="node12",
-            type=ImageType.STANDARD,
-            host="docker-registry.tools.wmflabs.org",
-            path="toolforge-node12-sssd-base",
-            tag="latest",
-            aliases=["tf-node12", "tf-node12-DEPRECATED"],
-            state="deprecated",
-            digest="",
-        ),
+        "alias for node16 image",
+        [
+            "tf-node16",
+            Image(
+                short_name="node16",
+                type=ImageType.STANDARD,
+                host="docker-registry.tools.wmflabs.org",
+                path="toolforge-node16-sssd-base",
+                tag="latest",
+                aliases=["tf-node16"],
+                state="stable",
+            ),
+        ],
     ],
     [
-        "php7.3",
-        Image(
-            short_name="php7.3",
-            type=ImageType.STANDARD,
-            host="docker-registry.tools.wmflabs.org",
-            path="toolforge-php73-sssd-base",
-            tag="latest",
-            aliases=["tf-php73", "tf-php73-DEPRECATED"],
-            digest="",
-            state="deprecated",
-        ),
+        "short name for php7.3 image",
+        [
+            "php7.3",
+            Image(
+                short_name="php7.3",
+                type=ImageType.STANDARD,
+                host="docker-registry.tools.wmflabs.org",
+                path="toolforge-php73-sssd-base",
+                tag="latest",
+                aliases=["tf-php73", "tf-php73-DEPRECATED"],
+                state="deprecated",
+            ),
+        ],
     ],
     [
-        "tool-some-tool/some-container:latest",
-        Image(
-            short_name="tool-some-tool/some-container:latest",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-some-tool/some-container",
-            tag="latest",
-            aliases=[
-                "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
-            ],
-            digest="",
-            state="stable",
-        ),
+        "short name for php8.4 image",
+        [
+            "php8.4",
+            Image(
+                short_name="php8.4",
+                type=ImageType.STANDARD,
+                host="docker-registry.svc.toolforge.org",
+                path="toolforge-php84-sssd-base",
+                tag="latest",
+                state="stable",
+            ),
+        ],
     ],
     [
-        "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-        Image(
-            short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-some-tool/some-container",
-            tag="latest",
-            aliases=[
-                "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
-            ],
-            digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-            state="stable",
-        ),
+        "buildpack image with default tag, no host and no digest",
+        [
+            "tool-some-tool/some-container:latest",
+            Image(
+                short_name="tool-some-tool/some-container:latest",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                state="stable",
+            ),
+        ],
     ],
     [
-        "tool-some-tool/some-container:stable",
-        Image(
-            short_name="tool-some-tool/some-container:stable",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-some-tool/some-container",
-            tag="stable",
-            aliases=[
-                "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
-            ],
-            digest="",
-            state="stable",
-        ),
+        "buildpack image with default tag and host, no digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-some-tool/some-container:latest",
+            Image(
+                short_name="tool-some-tool/some-container:latest",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                state="stable",
+            ),
+        ],
     ],
     [
-        "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
-        Image(
-            short_name="tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-some-tool/some-container",
-            tag="stable",
-            aliases=[
-                "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
-            ],
-            digest="sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
-            state="stable",
-        ),
+        "buildpack image with default tag and digest, no host",
+        [
+            "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+            Image(
+                short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                state="stable",
+            ),
+        ],
     ],
     [
-        "tool-other/tagged:example",
-        Image(
-            short_name="tool-other/tagged:example",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-other/tagged",
-            tag="example",
-            aliases=[
-                "tool-other/tagged:example@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
-            ],
-            digest="",
-            state="stable",
-        ),
+        "buildpack image with host, default tag and digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+            Image(
+                short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                state="stable",
+            ),
+        ],
     ],
     [
-        "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-        Image(
-            short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-            type=ImageType.BUILDPACK,
-            host=FAKE_HARBOR_HOST,
-            path="tool-some-tool/some-container",
-            tag="latest",
-            aliases=[
-                "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
-            ],
-            digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
-            state="stable",
-        ),
+        "buildpack image with different tag, no host and no digest",
+        [
+            "tool-some-tool/some-container:stable",
+            Image(
+                short_name="tool-some-tool/some-container:stable",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="stable",
+                aliases=[
+                    "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
+                ],
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image with different tag and host, no digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-some-tool/some-container:stable",
+            Image(
+                short_name="tool-some-tool/some-container:stable",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="stable",
+                aliases=[
+                    "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
+                ],
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image with different tag and digest, no host",
+        [
+            "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+            Image(
+                short_name="tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="stable",
+                aliases=[
+                    "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
+                ],
+                digest="sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image with host, different tag and digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+            Image(
+                short_name="tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="stable",
+                aliases=[
+                    "tool-some-tool/some-container:stable@sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81"
+                ],
+                digest="sha256:459de5f5ced49e4c8a104713a8a90a6b409a04f8894e1bc78340e4a8d76aed81",
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image of another tool with tag, no host and no digest",
+        [
+            "tool-other/tagged:example",
+            Image(
+                short_name="tool-other/tagged:example",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-other/tagged",
+                tag="example",
+                aliases=[
+                    "tool-other/tagged:example@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image of another tool with host and tag, no digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-other/tagged:example",
+            Image(
+                short_name="tool-other/tagged:example",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-other/tagged",
+                tag="example",
+                aliases=[
+                    "tool-other/tagged:example@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image with default tag, no host and digest",
+        [
+            "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+            Image(
+                short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                state="stable",
+            ),
+        ],
+    ],
+    [
+        "buildpack image with host, default tag and digest",
+        [
+            f"{FAKE_HARBOR_HOST}/tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+            Image(
+                short_name="tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                type=ImageType.BUILDPACK,
+                host=FAKE_HARBOR_HOST,
+                path="tool-some-tool/some-container",
+                tag="latest",
+                aliases=[
+                    "tool-some-tool/some-container:latest@sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3"
+                ],
+                digest="sha256:5b8c5641d2dbd7d849cacb39853141c00b29ed9f40af9ee946b6a6a715e637c3",
+                state="stable",
+            ),
+        ],
     ],
 ]
 
-# Build a set of tests with and without the harbor registry prefix, based on IMAGE_VARIANTS_TESTS
-IMAGE_NAME_TESTS_INCLUDING_REGISTRY_PREFIX = [
-    [provided_name, expected_name, expected_image]
-    for expected_name, expected_image in IMAGE_NAME_TESTS
-    for provided_name in (
-        [expected_name, f"{FAKE_HARBOR_HOST}/{expected_name}"]
-        if expected_image.type == ImageType.BUILDPACK
-        else [expected_name]
-    )
-]
 
-
-@pytest.mark.parametrize(
-    ["provided_name", "expected_name", "expected_image"],
-    IMAGE_NAME_TESTS_INCLUDING_REGISTRY_PREFIX,
+@cases(
+    "provided_name,expected_image",
+    *IMAGE_NAME_TESTS,
 )
-def test_from_short_name_or_url_happy_path(
-    fake_images, provided_name, expected_name, expected_image
-):
+def test_from_short_name_or_url_happy_path(fake_images, provided_name, expected_image):
+    full_url = expected_image.to_full_url()
+    expected_image_json = expected_image.model_dump(exclude_unset=True)
     gotten_image = Image.from_short_name_or_url(url_or_name=provided_name, tool_name="some-tool")
-    assert gotten_image == expected_image
+    assert expected_image_json == gotten_image.model_dump(exclude_unset=True)
 
-
-def test_from_short_name_or_url_raises_value_error_when_not_found_if_passing_rase_for_nonexisting(
-    fake_images,
-):
-    gotten_image = Image.from_short_name_or_url(url_or_name="invalid", tool_name="some-tool")
-
-    assert not gotten_image.exists
-
-
-@pytest.mark.parametrize(
-    ["provided_name", "expected_name", "expected_image"],
-    IMAGE_NAME_TESTS_INCLUDING_REGISTRY_PREFIX,
-)
-def test_from_short_name_or_url_using_to_full_url_happy_path(
-    fake_images, provided_name, expected_name, expected_image: Image
-):
-    image = Image.from_short_name_or_url(
-        url_or_name=expected_image.to_full_url(),
-        tool_name="some-tool",
-    )
-    assert image.short_name == expected_name or expected_name in image.aliases
+    gotten_image = Image.from_short_name_or_url(url_or_name=full_url, tool_name="some-tool")
+    assert expected_image_json == gotten_image.model_dump(exclude_unset=True)
 
 
 @cases(
@@ -203,6 +311,7 @@ def test_from_short_name_or_url_using_to_full_url_happy_path(
                 path="randomimage",
                 tag="randomtag",
                 exists=False,
+                state="unknown",
             ),
             "docker-registry.idontexist/randomimage:randomtag",
         ],
@@ -221,6 +330,7 @@ def test_from_short_name_or_url_using_to_full_url_happy_path(
                     "randomimage:randomtag@sha:1234567890123345678901234",
                 ],
                 exists=False,
+                state="unknown",
             ),
             "docker-registry.idontexist/randomimage:randomtag@sha:1234567890123345678901234",
         ],
@@ -235,6 +345,7 @@ def test_from_short_name_or_url_using_to_full_url_happy_path(
                 path="python1.5",
                 tag="",
                 exists=False,
+                state="unknown",
             ),
             "python1.5",
         ],
@@ -247,4 +358,6 @@ def test_from_short_name_or_url_non_existing_image_without_raising(
         url_or_name=short_name,
         tool_name="some-tool",
     )
-    assert gotten_image.model_dump() == expected_image.model_dump()
+    assert gotten_image.model_dump(exclude_unset=True) == expected_image.model_dump(
+        exclude_unset=True
+    )
